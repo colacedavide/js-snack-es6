31dfcff9a1b9e75b86106e9d8fee15e48fcea5ac
colacedavide <colace.davide00@gmail.com>
2025-12-15 15:23:44 +0100

    snak2

---
+++ snak2/script.js
@@ -0,0 +1,38 @@
+const squadre = [
+    {
+        nome: "squadra-uno",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "squadra-due",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "squadra-tre",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "squadra-quattro",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "squadra5-cinque",
+        punti: 0,
+        falli: 0
+    },
+    {
+        nome: "squadra-sei",
+        punti: 0,
+        falli: 0
+    },
+
+]
+
+function Random(array){
+    
+
+}
